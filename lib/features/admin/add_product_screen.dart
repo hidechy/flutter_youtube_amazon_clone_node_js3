0a4cd9b7160535ce2f2e0c0337b63117f291aaaf
toyohide <hide.toyoda@gmail.com>
2023-10-09 20:40:27 +0900

    commit 20231009 画像セレクト作成

--- lib/features/admin/add_product_screen.dart
+++ lib/features/admin/add_product_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
@@ -79,33 +80,51 @@ class _AddProductScreenState extends State<AddProductScreen> {
               child: Column(
                 children: [
                   const SizedBox(height: 20),
-                  GestureDetector(
-                    onTap: selectImages,
-                    child: DottedBorder(
-                      borderType: BorderType.RRect,
-                      radius: const Radius.circular(10),
-                      dashPattern: const [10, 4],
-                      strokeCap: StrokeCap.round,
-                      child: Container(
-                        width: double.infinity,
-                        height: 150,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Icon(Icons.folder_open, size: 40),
-                            const SizedBox(height: 15),
-                            Text(
-                              'Image Select',
-                              style: TextStyle(fontSize: 15, color: Colors.grey.shade400),
+                  images.isNotEmpty
+                      ? CarouselSlider(
+                          items: images.map(
+                            (i) {
+                              return Builder(
+                                builder: (BuildContext context) => Image.file(
+                                  i,
+                                  fit: BoxFit.cover,
+                                  height: 200,
+                                ),
+                              );
+                            },
+                          ).toList(),
+                          options: CarouselOptions(
+                            viewportFraction: 1,
+                            height: 200,
+                          ),
+                        )
+                      : GestureDetector(
+                          onTap: selectImages,
+                          child: DottedBorder(
+                            borderType: BorderType.RRect,
+                            radius: const Radius.circular(10),
+                            dashPattern: const [10, 4],
+                            strokeCap: StrokeCap.round,
+                            child: Container(
+                              width: double.infinity,
+                              height: 150,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const Icon(Icons.folder_open, size: 40),
+                                  const SizedBox(height: 15),
+                                  Text(
+                                    'Image Select',
+                                    style: TextStyle(fontSize: 15, color: Colors.grey.shade400),
+                                  ),
+                                ],
+                              ),
                             ),
-                          ],
+                          ),
                         ),
-                      ),
-                    ),
-                  ),
                   const SizedBox(height: 30),
                   CustomTextField(
                     controller: productNameController,
