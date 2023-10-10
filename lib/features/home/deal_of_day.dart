d7eba5b891cd0ada364d6b9a96772c7b9c3086eb
toyohide <hide.toyoda@gmail.com>
2023-10-10 18:53:47 +0900

    commit 20231010 DealOfDayを表示する

--- lib/features/home/deal_of_day.dart
+++ lib/features/home/deal_of_day.dart
@@ -1,6 +1,9 @@
-// ignore_for_file: strict_raw_type
-
 import 'package:flutter/material.dart';
+
+import '../../common/loader.dart';
+import '../../models/product.dart';
+import '../product_detail/product_detail_screen.dart';
+import 'home_service.dart';
 
 class DealOfDay extends StatefulWidget {
   const DealOfDay({super.key});
@@ -10,71 +13,74 @@ class DealOfDay extends StatefulWidget {
 }
 
 class _DealOfDayState extends State<DealOfDay> {
-  List list = [
-    'http://toyohide.work/BrainLog/public/UPPHOTO/2023/2023-09-27/20230927_131236144.jpg',
-    'http://toyohide.work/BrainLog/public/UPPHOTO/2023/2023-09-27/20230927_131522177.jpg',
-    'http://toyohide.work/BrainLog/public/UPPHOTO/2023/2023-09-27/20230927_131529638.jpg',
-    'http://toyohide.work/BrainLog/public/UPPHOTO/2023/2023-09-27/20230927_131551309.jpg',
-  ];
+  Product? product;
 
+  final HomeService homeService = HomeService();
+
+  ///
   @override
   void initState() {
     super.initState();
+
+    fetchDealOfDay();
   }
 
+  ///
+  Future<void> fetchDealOfDay() async {
+    product = await homeService.fetchDealOfDay(context: context);
+
+    setState(() {});
+  }
+
+  ///
+  void navigateToDetailScreen() => Navigator.pushNamed(context, ProductDetailScreen.routeName, arguments: product);
+
+  ///
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          alignment: Alignment.topLeft,
-          padding: const EdgeInsets.only(left: 10, top: 15),
-          child: const Text(
-            'Deal of the day',
-            style: TextStyle(fontSize: 20),
-          ),
-        ),
-        Image.network('http://toyohide.work/BrainLog/public/UPPHOTO/2023/2023-09-27/20230927_131236144.jpg'),
-        Container(
-          padding: const EdgeInsets.only(left: 15),
-          alignment: Alignment.topLeft,
-          child: const Text(
-            '\$100',
-            style: TextStyle(fontSize: 18),
-          ),
-        ),
-        Container(
-          alignment: Alignment.topLeft,
-          padding: const EdgeInsets.only(left: 15, top: 5, right: 40),
-          child: const Text(
-            'hidechy',
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis,
-          ),
-        ),
-        SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: list
-                  .map(
-                    (e) => Image.network(e, fit: BoxFit.fitWidth, width: 100, height: 100),
-                  )
-                  .toList()),
-        ),
-        Container(
-          padding: const EdgeInsets.symmetric(
-            vertical: 15,
-          ).copyWith(left: 15),
-          alignment: Alignment.topLeft,
-          child: Text(
-            'See all deals',
-            style: TextStyle(
-              color: Colors.cyan[800],
-            ),
-          ),
-        ),
-      ],
-    );
+    return product == null
+        ? const Loader()
+        : product!.name.isEmpty
+            ? const SizedBox()
+            : GestureDetector(
+                onTap: navigateToDetailScreen,
+                child: Column(
+                  children: [
+                    Container(
+                      alignment: Alignment.topLeft,
+                      padding: const EdgeInsets.only(left: 10, top: 15),
+                      child: const Text('Deal of the day', style: TextStyle(fontSize: 20)),
+                    ),
+                    Image.network(product!.images[0], height: 235, fit: BoxFit.fitHeight),
+                    Container(
+                      padding: const EdgeInsets.only(left: 15),
+                      alignment: Alignment.topLeft,
+                      child: Text(product!.price.toString(), style: const TextStyle(fontSize: 18)),
+                    ),
+                    Container(
+                      alignment: Alignment.topLeft,
+                      padding: const EdgeInsets.only(left: 15, top: 5, right: 40),
+                      child: Text(product!.name, maxLines: 2, overflow: TextOverflow.ellipsis),
+                    ),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: product!.images
+                            .map((e) => Image.network(e, fit: BoxFit.fitWidth, width: 100, height: 100))
+                            .toList(),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.symmetric(vertical: 15).copyWith(left: 15),
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        'See all deals',
+                        style: TextStyle(color: Colors.cyan[800]),
+                      ),
+                    ),
+                  ],
+                ),
+              );
   }
 }
